84b5aaea961aa3444cf96165e2235e05d95b2132
kevinkassimo <kevinkassimo@gmail.com>
2016-06-05 00:24:42 -0700

    Create SimpleSymbols.js

---
+++ Javascript/Coderbyte_challenges/Easy/SimpleSymbols.js
@@ -0,0 +1,38 @@
+//TODO: MY_SOL
+function SimpleSymbols(str) {
+	str = '=' + str + '=';
+	var list = str.split('+');
+	var all = str.match(/[A-Za-z]/g).length;
+	var num = 0;
+	list.forEach(function(part) {
+		if (part != undefined && part != null && part.length != 0) {
+			if (isalpha(part[0])) {
+				console.log(part)
+				if (part.length != 1) {
+					return false;
+				}
+				num++;
+			}
+		}
+	});
+	if (all !== num) {
+		return false;
+	}
+	return true;
+}
+
+function isalpha(chr) {
+	if (chr.length != 1) {
+		return false;
+	}
+	if (chr.match(/[A-Za-z]/)) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+//TODO: BEST_SOL
+function simpleSymbols(str){
+  return str.match(/\+[a-z]\+/gi).length === str.match(/[a-z]/gi).length;
+}
