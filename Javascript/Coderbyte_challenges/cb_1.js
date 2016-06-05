884527004d0ec5870f1b81206c4fd3b1b94c0e7c
kevinkassimo <kevinkassimo@gmail.com>
2016-06-04 22:32:01 -0700

    Update cb_1.js

--- Javascript/Coderbyte_challenges/cb_1.js
+++ Javascript/Coderbyte_challenges/cb_1.js
@@ -1,3 +1,5 @@
+//TODO: MY_SOL
+
 function FirstReverse(str) { 
   // code goes here
   var new_str = '';
@@ -8,3 +10,15 @@ function FirstReverse(str) {
 }
 // keep this function call here 
 FirstReverse(readline());   
+
+//TODO: BEST_SOL
+function FirstReverse(str) { 
+
+  // first we split the string which creates an array of characters, e.g. ['c','a','t']
+  // then we call the reverse function on this array 
+  // and finally we turn the reversed array back into a string using the join function
+  return str.split('').reverse().join('');
+         
+}
+   
+FirstReverse("I am a string that needs to be reversed");   
