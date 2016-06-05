8dad58f5006071993ee8b8d6ad8168aaf4a53e57
kevinkassimo <kevinkassimo@gmail.com>
2016-06-04 23:19:54 -0700

    Create SimpleAdding.js

---
+++ Javascript/Coderbyte_challenges/SimpleAdding.js
@@ -0,0 +1,13 @@
+function SimpleAdding(num) { 
+
+    var result = 0;
+    for (var i = 1; i <= num; i++) {
+        result += i;
+    }
+  // code goes here  
+  return result; 
+         
+}
+   
+// keep this function call here 
+SimpleAdding(readline());
