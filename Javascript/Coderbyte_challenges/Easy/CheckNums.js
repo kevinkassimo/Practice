08cac350035a379dbcda55e487ec6f98f486706d
kevinkassimo <kevinkassimo@gmail.com>
2016-06-05 00:31:52 -0700

    Update CheckNums.js

--- Javascript/Coderbyte_challenges/Easy/CheckNums.js
+++ Javascript/Coderbyte_challenges/Easy/CheckNums.js
@@ -1,3 +1,9 @@
 //TODO: MY_SOL
 function CheckNums(num1, num2) {
+	return (num1 === num2) ? "-1" : (num2 > num1);
+}
+
+//TODO: BEST_SOL
+function CheckNums(num1, num2){
+  return (num1 === num2) ? -1 : (num2 > num1);
 }
